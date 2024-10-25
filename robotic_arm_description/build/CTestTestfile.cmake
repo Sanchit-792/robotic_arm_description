6303d6f65238413866d7f17258b2191c68c53090
Sanchit-792 <sanchitpawar792@gmail.com>
2024-10-25 13:04:52 +0530

    add moveit controls

---
+++ robotic_arm_description/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sanchit/catkin_ws/src/robotic_arm_description/robotic_arm_description
+# Build directory: /home/sanchit/catkin_ws/src/robotic_arm_description/robotic_arm_description/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ robotic_arm_description/build/gtest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest
+# Build directory: /home/sanchit/catkin_ws/src/robotic_arm_description/robotic_arm_description/build/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
